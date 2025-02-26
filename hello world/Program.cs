1ef3f0e08efc3303ca1c4d60d9469eb061320ad4
williamsoga <williamhsoga@gmail.com>
2025-02-26 11:35:03 -0300

    adicionado o arquivo hello world

---
+++ hello world/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace Teste_1
+{
+    class Prgram
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine("Hello World");
+            Console.WriteLine("Programar em c# é muito legal");
+
+
+        }
+
+
+    }
+}
